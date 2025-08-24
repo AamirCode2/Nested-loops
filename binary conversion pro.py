d2930807e15ca2d0efebe3fc3760b8f59c3a63ac
AamirCode2 <aamirlovesart@gmail.com>
2025-08-24 12:47:19 +1000

    Add files via upload

---
+++ binary conversion pro.py
@@ -0,0 +1,5 @@
+n = int(input("Enter an integer to find its binary representation: "))
+
+while n != 0:
+    n = n//2
+    print(n)
---
+++ testing.py
@@ -0,0 +1,3 @@
+n = str(input("Enter name: "))
+
+print("hi {0} G.M".format(n))
