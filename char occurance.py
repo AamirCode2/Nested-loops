371fe8b5ed3b13f5db4a89474b44a0285636cc57
AamirCode2 <aamirlovesart@gmail.com>
2025-08-24 12:46:26 +1000

    Add files via upload

---
+++ char occurance.py
@@ -0,0 +1,11 @@
+str = input("Please enter your own word: ")
+char = input("Please enter your own character: ")
+i = 0
+count = 0
+
+while(i < len(str)):
+    if (str[i] == char):
+          count = count + 1
+    i = i +1
+
+print("The total number of times %s has occurred = %i" %(char, count))
